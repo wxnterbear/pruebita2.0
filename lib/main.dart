dce08d6174066718b88f23bd76e41860f6085218
wxnterbear <natamenro511@gmail.com>
2024-04-05 01:11:15 -0500

    done

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+
+      home: Scaffold(
+
+        appBar: AppBar(
+
+          title: const Text("Mi primera aplicación en Flutter"),
+
+        ),
+
+        body: const Center(
+
+          child: Text("¡Hola mundo!"),
+
+        ),
+
+      ),
+
+    );
+
+  }
+
+}
+
+
+
